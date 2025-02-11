f5ea8cd68d7f65baec5aa0643e0fa507c28696ff
Kisejjere Rashid <88730649+rashidkisejjere0784@users.noreply.github.com>
2025-02-11 11:02:41 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("requirements.txt", "r", encoding='utf-16') as f:
     install_requires = f.readlines()
 
 setuptools.setup(
-    version=__version__,
+    version="0.0.1",
     install_requires=install_requires,
     long_description=long_description,
     long_description_content_type="text/markdown",
